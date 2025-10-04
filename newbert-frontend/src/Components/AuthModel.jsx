efe108c4be3b12a31ea9095a9bc73dc40a473b73
anantkpatel <imagine231216@gmail.com>
2025-10-04 17:50:52 +0530

    kjoi

--- newbert-frontend/src/Components/AuthModel.jsx
+++ newbert-frontend/src/Components/AuthModel.jsx
@@ -49,6 +49,15 @@ function AuthModal({ isOpen, onClose }) {
             {isSignUp ? "Sign Up" : "Sign In"}
           </button>
         </form>
+           <p className="mt-4 text-sm text-center  text-black">
+          {isSignUp ? "Already have an account?" : "Don't have an account?"}{" "}
+          <span
+            className="text-blue-600 cursor-pointer"
+            onClick={() => setIsSignUp(!isSignUp)}
+          >
+            {isSignUp ? "Sign In" : "Sign Up"}
+          </span>
+        </p>
 
         {/* Divider */}
         <div className="flex items-center my-4">
@@ -58,13 +67,13 @@ function AuthModal({ isOpen, onClose }) {
         </div>
 
         {/* Google Auth */}
-        <div className="w-full bg-blue-600 h-[10vh] flex items-center justify-between ">
+        <div className="w-full h-[10vh] flex items-center justify-between ">
             
             <button className="w-[50%] border border-gray-400 rounded-full flex items-center justify-center  hover:bg-gray-100 transition">
           <img
-            src="https://www.citypng.com/public/uploads/preview/google-logo-icon-gsuite-hd-701751694791470gzbayltphh.png"
-            alt="Google"
-            className="contain h-9 w-9"
+            src="https://download.logo.wine/logo/GitHub/GitHub-Logo.wine.png"
+            alt="Github"
+            className="contain h-12 w-15"
           />
           
         </button>
@@ -72,7 +81,7 @@ function AuthModal({ isOpen, onClose }) {
           <img
             src="https://www.citypng.com/public/uploads/preview/google-logo-icon-gsuite-hd-701751694791470gzbayltphh.png"
             alt="Google"
-            className="contain h-9 w-9"
+            className="contain h-11 w-15"
           />
           
         </button>
