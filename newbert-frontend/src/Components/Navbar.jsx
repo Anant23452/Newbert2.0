f86368eb09618db2b9132203d9f143c907758f41
anantkpatel <imagine231216@gmail.com>
2026-04-02 21:32:37 +0530

    com

--- newbert-frontend/src/Components/Navbar.jsx
+++ newbert-frontend/src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 function Navbar() {
   //mobile view ke liye
   const [modelOpen, setModelOpen] = useState(false);
-
+2
   //login ke liye
   const [showAuth, setShowAuth] = useState(false);
 
@@ -26,24 +26,22 @@ function Navbar() {
     <>
       <nav className="w-full h-[10vh]  flex justify-center items-center px-4 py-9 border-b-2 border-zinc-600">
         <div className="logo flex items-center gap-1  w-full">
-          <img
-            src="/src/assets/2-removebg-preview(1).png"
-            alt="Newbert Logo"
-            className="select-none h-[4rem] w-auto object-contain"
-          />
-          <h1 className="text-3xl font-bold select-none ">Newbert</h1>
+         
+          <h1 className="text-2xl font-bold select-none ">
+            
+         New<span className="text-orange-500">bert</span></h1>
         </div>
 
         <div className="w-full h-full  flex  items-center  gap-4 px-4 ">
           <div className="link text-white flex gap-10 px-8 items-center p-2">
             {[
               "Home",
-              "Academics",
-              "Carrer",
-              "Community",
-              "About",
-              "sell",
-              "Request.call",
+              "Alumni wall",
+              "Jobs",
+              "Resume-AI",
+              "Courses",
+              "Myroadmap",
+              "LeaderBoard",
             ].map((item, index) => {
               return (
                 <a
@@ -63,12 +61,14 @@ function Navbar() {
                "
               onClick={() => navigate("/dashboard")}
                >
+                <h4>Current streke</h4>
                 <img
                   src="https://i.pinimg.com/1200x/f0/38/38/f038383985e6289f4c208150818e01ab.jpg"
                   alt="img"
                   className="w-full h-full object-cover  "
                 />
                 <button onClick={()=>navigate("/dashboard") } >save me</button>
+                
               </div>
             ) : (
               <button
