352c7e832848d148fd9b0e742fe6a998da93b474
anantkpatel <imagine231216@gmail.com>
2026-07-12 23:50:35 +0530

    comit Roadmap

--- newbert-frontend/src/Routing.jsx
+++ newbert-frontend/src/Routing.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Routes,Route } from 'react-router-dom';
 import AllumniWall from './pages/AlumniWall';
 import Profile from './pages/Profile';
-import Roadmap from './pages/Temp';
+import Roadmap from './pages/Roadmap';
 import Jobs from './pages/Jobs';
 import Leaderboard from './pages/Leaderboard';
 import ResumeAi from './pages/ResumeAi';
